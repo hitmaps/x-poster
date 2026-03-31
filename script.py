dd424aa50186b7f2a61a6b1df625dcffd7c6a353
Mike Koch <mkoch227@gmail.com>
2026-03-31 14:36:52 +0000

    Edit script.py

--- script.py
+++ script.py
@@ -1,3 +1,29 @@
+# MONKEY PATCH: Remove this block when twikit is updated to fix ON_DEMAND_FILE_REGEX
+import re
+_tx_mod = __import__('twikit.x_client_transaction.transaction', fromlist=['ClientTransaction'])
+_tx_mod.ON_DEMAND_FILE_REGEX = re.compile(
+    r""",(\d+):["']ondemand\.s["']""", flags=(re.VERBOSE | re.MULTILINE))
+_tx_mod.ON_DEMAND_HASH_PATTERN = r',{}:"([0-9a-f]+)"'
+
+async def _patched_get_indices(self, home_page_response, session, headers):
+    key_byte_indices = []
+    response = self.validate_response(home_page_response) or self.home_page_response
+    on_demand_file_index = _tx_mod.ON_DEMAND_FILE_REGEX.search(str(response)).group(1)
+    regex = re.compile(_tx_mod.ON_DEMAND_HASH_PATTERN.format(on_demand_file_index))
+    filename = regex.search(str(response)).group(1)
+    on_demand_file_url = f"https://abs.twimg.com/responsive-web/client-web/ondemand.s.{filename}a.js"
+    on_demand_file_response = await session.request(method="GET", url=on_demand_file_url, headers=headers)
+    key_byte_indices_match = _tx_mod.INDICES_REGEX.finditer(str(on_demand_file_response.text))
+    for item in key_byte_indices_match:
+        key_byte_indices.append(item.group(2))
+    if not key_byte_indices:
+        raise Exception("Couldn't get KEY_BYTE indices")
+    key_byte_indices = list(map(int, key_byte_indices))
+    return key_byte_indices[0], key_byte_indices[1:]
+
+_tx_mod.ClientTransaction.get_indices = _patched_get_indices
+# END MONKEY PATCH
+
 import asyncio
 from datetime import datetime
 from twikit import Client
